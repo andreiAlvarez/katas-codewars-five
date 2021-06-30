ccb6682d52d6b9d39cfe798c50e1725a1a98daca
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-06-30 18:39:46 -0400

    Update kata.js

--- kata.js
+++ kata.js
@@ -106,3 +106,38 @@ const CaesarCipher = function (shift) {
   this.decode = str =>
     code(str, 26 - shift);
 };
+
+// kata 9 
+
+const
+  id = x => x,
+  number = x => (f = id) => f(x),
+  [zero, one, two, three, four, five, six, seven, eight, nine] =
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(number),
+  plus = x => y => y + x,
+  minus = x => y => y - x,
+  times = x => y => y * x,
+  dividedBy = x => y => y / x;
+
+// solution 2
+
+var n = function(digit) {
+  return function(op) {
+    return op ? op(digit) : digit;
+  }
+};
+var zero = n(0);
+var one = n(1);
+var two = n(2);
+var three = n(3);
+var four = n(4);
+var five = n(5);
+var six = n(6);
+var seven = n(7);
+var eight = n(8);
+var nine = n(9);
+
+function plus(r) { return function(l) { return l + r; }; }
+function minus(r) { return function(l) { return l - r; }; }
+function times(r) { return function(l) { return l * r; }; }
+function dividedBy(r) { return function(l) { return l / r; }; }
