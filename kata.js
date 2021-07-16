1dbf117ae6ff4a63edf7dea3785d2d50fb6a0300
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2021-07-16 17:58:51 -0400

    Update kata.js

--- kata.js
+++ kata.js
@@ -183,3 +183,16 @@ function ipToInt32(ip) {
     return new Array(9 - bin.length).join('0') + bin;
   }).join(''), 2);
 }
+
+// kata 14 
+
+const solve = (map, miner, exit, path=[])=> {
+  if (!map[miner.x] || !map[miner.x][miner.y]) return false;
+  if (miner.x == exit.x && miner.y == exit.y) return path;
+  map = map.map(v => v.slice());
+  map[miner.x][miner.y] = false;
+  return solve(map, {x: miner.x    , y: miner.y - 1}, exit, path.concat('up')) ||
+         solve(map, {x: miner.x    , y: miner.y + 1}, exit, path.concat('down')) ||
+         solve(map, {x: miner.x - 1, y: miner.y    }, exit, path.concat('left')) ||
+         solve(map, {x: miner.x + 1, y: miner.y    }, exit, path.concat('right'));
+}
